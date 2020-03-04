85dc9e473c63e9d7a8ace6e4954f7f278b1d5615
Matuk <mateusmatuk@fai-mg.br>
2020-03-04 20:46:43 -0300

    2o commit

--- src/br/fai/projeto04/Main.java
+++ src/br/fai/projeto04/Main.java
@@ -1,10 +1,49 @@
 package br.fai.projeto04;
 
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
+		Main app = new Main();
+		app.start();
+		
 	}
 
+	private void start() {
+		
+		 usandoWhile();
+		 usandoDoWhile();
+		 interacaoFor();
+	            
+	        
+		
+	}
+
+	private void usandoWhile() {
+		
+         int i = 0;
+        
+        System.out.println("\nusando while.\n");
+        while(i < 20){
+            
+            System.out.println("Valor i: " + i);
+            i++;
+		
+	}
+
+    private void usandoDoWhile() {
+     
+    	int i = 0;
+    	
+    	
+     
+    }
+    	
+	
+	private void interacaoFor() {
+		
+		
+	}
 }
